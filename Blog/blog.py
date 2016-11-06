f29d106181619447ee37e171cdd033e1020f6ca1
Mike Fang <mike1026915.cs96@g2.nctu.edu.tw>
2016-11-06 22:42:42 +0800

    1. Add blog function and give permalink

--- Blog/blog.py
+++ Blog/blog.py
@@ -1,6 +1,9 @@
 import webapp2
 import jinja2
+import logging
 import os
+
+from google.appengine.ext import db
 
 template_dir = os.path.join(os.path.dirname(__file__), 'template')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir), autoescape=True)
@@ -16,28 +19,54 @@ class Handler(webapp2.RequestHandler):
     def render(self, template, **kw):
         self.write(self.render_str(template, **kw))
 
+class Blog(db.Model):
+    subject = db.StringProperty(required=True)
+    content = db.TextProperty(required=True)
+    created = db.DateTimeProperty(auto_now_add=True)
+
 class MainPage(Handler):
     def get(self):
         self.response.headers['Content-Type'] = 'text/html'
-        self.render("main.html", output='') 
- 
+        self.get_ten_records()
+        
+    def get_ten_records(self):
+        blogs = db.GqlQuery("SELECT * FROM Blog")
+        self.render("main.html", blogs=blogs.run())
+        
 
+class AddNewBlog(Handler):
+    def get(self):
+        self.response.headers['Content-Type'] = 'text/html'
+        self.render("new_form.html", output='')    
+        
+
+
+
+class SaveBlog(Handler):
+ 
     def post(self):
         self.response.headers['Content-Type'] = 'text/html'
-        input_data = self.request.get("text")
-        self.render("main.html", output=self.rot13(input_data))     
-        
-    def rot13(self, input_data):
-        new_data = []
-        for c in input_data:
-            if c.isalpha():
-                base_number = ord('a') if c.islower() else ord('A')
-                c_num = ord(c) - base_number
-                c_num += 13
-                c_num %= 26
-                new_data.append(chr(c_num+base_number))
-            else:
-                new_data.append(c)
-        return ''.join(new_data)
+        subject = self.request.get("subject")
+        content = self.request.get("content")
+        if subject and content:
+            blog = Blog(subject=subject, content=content)
+            blog.put()
 
-app = webapp2.WSGIApplication([('/', MainPage),], debug=True)
+        self.redirect("/")
+        #self.response.out.write("Success: " + subject + " " + content)
+        
+    def save_to_database(self, input_data):
+        self.response.out.write("YES")
+
+
+class PageHandler(Handler):
+    def get(self, post_id):
+        logging.info(dir(Blog))
+        p = Blog.get_by_id(int(post_id))
+        p = [p,]
+        self.response.headers['Content-Type'] = 'text/html'
+        self.render("main.html", blogs=p) 
+        
+
+app = webapp2.WSGIApplication([('/', MainPage),('/new_form', AddNewBlog),
+                               ('/save_blog', SaveBlog), ('/blog/([0-9]+)', PageHandler)], debug=True)
